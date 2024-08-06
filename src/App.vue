431bfbe06252d34e44a1d88343406f17fad8ce7f
Yauheni <yauhenikozak@gmail.com>
2024-08-06 21:36:52 +0300

    fix

--- src/App.vue
+++ src/App.vue
@@ -130,7 +130,7 @@ function placePipes() {
     // 1 -> -128 - 256 (pipeHeight/4 - pipeHeight/2) = -3/4 pipeHeight
     let randomPipeY = pipeY.value - pipeHeight.value/4 - Math.random()*(pipeHeight.value/2);
     console.log('getRandomInRange(34, 2, 2)', getRandomInRange(14, 2, 2))
-    let openingSpace = board.value?.height/getRandomInRange(8, 1, 6);
+    let openingSpace = board.value?.height/getRandomInRange(8, 0, 6);
     console.log('openingSpace', openingSpace)
     let topPipe = {
         img : topPipeImg.value,
